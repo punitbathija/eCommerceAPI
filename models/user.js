2c8b24af6579d8f2fd49a3d0489385d8a6e138a1
Punit Bathija <punit644@gmail.com>
2023-02-11 15:51:50 +0530

    Encrypted the password using bcryptjs and used the webhook pre to encrypt the password before saving to db also checking the if password is modified or not

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -50,6 +51,13 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now(),
   },
+});
+
+// Encrypting password before saving the password onto the database
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 module.exports = mongoose("User", userSchema);
