bdd7833b2bfff1aa76734dd43cf097f1353eadd8
Punit Bathija <punit644@gmail.com>
2023-02-11 14:28:20 +0530

    Learnt about bigPomises and how to wrap everthing around it

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+
+// swagger documentation and swagger middleware
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// regular middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cookies and files middlewares
+app.use(cookieParser());
+app.use(fileUpload());
 
 // morgan middleware
 app.use(morgan("tiny"));
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,11 @@
-exports.home = (req, res) => {
+const BigPromise = require("../middlewares/bigPromise");
+
+exports.home = BigPromise((req, res) => {
   res.status(200).json({
     success: true,
     greeting: "Hello from the API",
   });
-};
+});
 
 exports.homeDummy = (req, res) => {
   res.status(200).json({
