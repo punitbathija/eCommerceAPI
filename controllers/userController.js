6d40b50ef2b7ce5c957167dff0bb867daaf27495
Punit Bathija <punit644@gmail.com>
2023-02-13 15:37:13 +0530

    Login route sucessfully designed and tested

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,6 +11,10 @@ exports.signup = bigPromise(async (req, res, next) => {
   // Convey to frontend engineer that the filename should be photo
   let result;
 
+  if (!req.files) {
+    return next(new CustomError("Please upload an image to continue", 400));
+  }
+
   if (req.files) {
     let file = req.files.photo;
     result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
@@ -34,5 +38,33 @@ exports.signup = bigPromise(async (req, res, next) => {
     },
   });
 
+  cookieToken(user, res);
+});
+
+exports.login = bigPromise(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // check if email and password are present
+  if (!email || !password) {
+    return next(new CustomError("Please enter email & password", 400));
+  }
+
+  // comparing email and password
+  const user = await User.findOne({ email }).select("+password");
+
+  // if user does not exsist
+  if (!user) {
+    return next(
+      new CustomError("Please enter registered email & password", 400)
+    );
+  }
+
+  const isPasswordCorrect = await user.validatePassword(password);
+
+  // if password or email is incorrect
+  if (!isPasswordCorrect) {
+    return next(new CustomError("Please enter correct email & password", 400));
+  }
+  // if everything goes well we send the token
   cookieToken(user, res);
 });
