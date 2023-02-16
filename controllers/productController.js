6ca8d009c0e8f64e80ef83d30a68dfbbbe19692e
Punit Bathija <punit644@gmail.com>
2023-02-16 16:31:15 +0530

    Added a route to add a review to a product If review exsists replace with the new review

--- controllers/productController.js
+++ controllers/productController.js
@@ -161,3 +161,42 @@ exports.adminDeleteProduct = bigPromise(async (req, res, next) => {
     message: "The following product was Deleted",
   });
 });
+
+exports.addReview = bigPromise(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  const previousReview = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  if (previousReview) {
+    product.reviews.forEach((review) => {
+      if (review.user.toString() === req.user._id.toString()) {
+        review.comment = comment;
+        review.rating = rating;
+      }
+    });
+  } else {
+    product.reviews.push(review);
+    product.numberOfReviews = product.reviews.length;
+  }
+
+  product.ratings =
+    product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    product.reviews.length;
+
+  await product.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+  });
+});
