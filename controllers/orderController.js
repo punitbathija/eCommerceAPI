3b4de2cd8ffa00358687009b64a948acd530fab0
Punit Bathija <punit644@gmail.com>
2023-02-17 15:08:28 +0530

    Added and tested a route to get single order Also added a method to add user name and email on request

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,7 @@
 const Order = require("../models/order");
 const Product = require("../models/product");
 const bigPromise = require("../middlewares/bigPromise");
+const CustomError = require("../utils/customError");
 
 exports.createOrder = bigPromise(async (req, res, next) => {
   const {
@@ -21,6 +22,22 @@ exports.createOrder = bigPromise(async (req, res, next) => {
     totalAmount,
     user: req.user._id,
   });
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+exports.getSingleOrder = bigPromise(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new CustomError("Please enter correct order id", 401));
+  }
 
   res.status(200).json({
     success: true,
--- routes/order.js
+++ routes/order.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { createOrder } = require("../controllers/orderController");
+const {
+  createOrder,
+  getSingleOrder,
+} = require("../controllers/orderController");
 const router = express.Router();
 const { isLoggedIn, customRole } = require("../middlewares/user");
 
 router.route("/order/create").post(isLoggedIn, createOrder);
+router.route("/order/:id").get(isLoggedIn, getSingleOrder);
 
 module.exports = router;
