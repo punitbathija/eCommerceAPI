0b56c62cab2f1119f8631b3fec51a9ba38b183a9
Punit Bathija <punit644@gmail.com>
2023-02-17 16:22:56 +0530

    Added a route for the admin to mark order as delivered

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,6 +2,7 @@ const Order = require("../models/order");
 const Product = require("../models/product");
 const bigPromise = require("../middlewares/bigPromise");
 const CustomError = require("../utils/customError");
+const order = require("../models/order");
 
 exports.createOrder = bigPromise(async (req, res, next) => {
   const {
@@ -59,14 +60,40 @@ exports.getLoggedinUserOrders = bigPromise(async (req, res, next) => {
 });
 
 exports.adminGetAllOrders = bigPromise(async (req, res, next) => {
-  const order = await Order.find().populate("user", "name email");
+  const orders = await Order.find().populate("user", "name email");
 
-  if (!order) {
-    return next(new CustomError("Please enter correct order id", 401));
+  if (!orders) {
+    return next(new CustomError("No orders to display", 401));
   }
 
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+exports.adminUpdateOrder = bigPromise(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order.orderStatus === "delivered") {
+    return next(new CustomError("Order is already marked as delivered", 401));
+  }
+
+  order.orderStatus = req.body.orderStatus;
+
+  order.orderItems.forEach(async (prod) => {
+    await updateProductStock(prod.product, prod.quantity);
+  });
+
+  await order.save();
   res.status(200).json({
     success: true,
     order,
   });
 });
+
+async function updateProductStock(productId, quantity) {
+  const product = await Product.findById(productId);
+  product.stock = product.stock - quantity;
+  await product.save({ validateBeforeSave: false });
+}
