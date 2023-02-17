167664c568bceb44b5f4abf6df04daccb2c73d65
Punit Bathija <punit644@gmail.com>
2023-02-17 14:31:28 +0530

    Added a controller to createOrder

---
+++ controllers/orderController.js
@@ -0,0 +1,29 @@
+const Order = require("../models/order");
+const Product = require("../models/product");
+const bigPromise = require("../middlewares/bigPromise");
+
+exports.createOrder = bigPromise(async (req, res, next) => {
+  const {
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    taxAmount,
+    shippingAmount,
+    totalAmount,
+  } = req.body;
+
+  const order = await Order.create({
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    taxAmount,
+    shippingAmount,
+    totalAmount,
+    user: req.user._id,
+  });
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
